ea44d70000d852c4d40e53ac0e34f816c6a900e9
IgorHulyaschy <gulyaschy@gmail.com>
2021-09-05 15:06:15 +0300

    added test

--- src/url/tests/test.js
+++ src/url/tests/test.js
@@ -1,101 +1,58 @@
-const request = require('supertest')
-const app = require('../../app')
-const Router = require("koa-router");
-const router = new Router();
-router.use("/", require("../router"));
-app.use(router.middleware());
-const req = request(app);
-// const bodyParser = require('koa-bodyparser')
-// const {save} = require('./save_json.json')
+const request = require('supertest');
+const app = require('../../app');
 
-describe('docs', () => {
-
+let server;
+beforeAll(async () => {
+  jest.setTimeout(10000);
+  server = app.callback();
+});
+afterAll((done) => {
+  done();
+});
+describe('GET - docs', () => {
   it('should return the docs web page', async () => {
-    const response = await req.get('docs')
-      .expect(response.status).toEqual(200)
-      .expect(response.type).toEqual('text/html');
+    const response = await request(server).get('/docs');
+    expect(response.status).toEqual(200);
   });
 });
 
+describe('POST - /api/url/shorten', () => {
+  it('should return already exsisting short URL', async () => {
+    const url = {
+      longUrl:
+        'https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0',
+    };
+    const response = await request(server).post('/api/url/shorten').send(url);
+    expect(response.body).toStrictEqual({
+      url: {
+        shortUrl: 'http://localhost:3000/8XTrYwxuT',
+      },
+    });
+  });
+  it('should return error - 400', async () => {
+    const url = {
+      longUrl: '123456',
+    };
+    const response = await request(server).post('/api/url/shorten').send(url);
+    expect(response.body).toStrictEqual({
+      message: 'Invalid long URL',
+    });
+  });
+  it('should return error - 500', async () => {
+    const url = {
+      longUrl: 1,
+    };
+    await request(server).post('/api/url/shorten').send(url).expect(500);
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// jest.mock('./get.json', () => {[
-//   {
-//     urlCode:"123",
-//     longUrl:"https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0",
-//     shortUrl: "http://localhost:3000/123"
-//   },
-//   {
-//     urlCode:"12123",
-//     longUrl:"https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0",
-//     shortUrl: "http://localhost:3000/12123"
-//   }
-// ]})
-// let urlCode;
-
-// let firstState;
-// describe("server-routes", () => {
-//   it("GET /states - success", async () => {
-//     const { body } = await request(app).get("/states"); //use the request function that we can use the app// save the response to body variable
-//     expect(body).toEqual([
-//       {
-//         state: "MI",
-//         capital: "Lansing",
-//         governor: "Gretchen Whitmer",
-//       },
-//       {
-//         state: "GA",
-//         capital: "Atlanta",
-//         governor: "Brian Kemp",
-//       },
-//     ]);
-//     firstState = body[0];
-//   });
-// it("GET /states/MI - success", async () => {
-//     const { body } = await request(app).get(`/states/${urlCode}`);
-//     expect(body).toEqual(urlCode);
-//   });
-// });
-
-// jest.mock("./save_json", () => ({
-//   save: jest.fn(),
-// }));
-
-// it("POST /states - success", async () => {
-  
-//   const { body } = await request(app).post("/api/url/shorten").send({
-//     longUrl: "https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0",
-//   });
-//   expect(body).toEqual();
-//   expect(save).toHaveBeenCalledWith([
-//     {
-//       urlCode:"123",
-//       longUrl:"https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0",
-//       shortUrl: "http://localhost:3000/123"
-//     },
-//     {
-//       urlCode:"12123",
-//       longUrl:"https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0",
-//       shortUrl: "http://localhost:3000/12123"
-//     }
-//   ]);
-// });
-
-
+describe('GET - /:urlCode', () => {
+  it('should redirect to longUrl', async () => {
+    const urlCode = '8XTrYwxuT';
+    await request(server).get(`/${urlCode}`).expect(302);
+  });
+  it('should return error - 500', async () => {
+    const urlCode = '8XTrY';
+    await request(server).get(`/${urlCode}`).expect(500);
+  });
+});
