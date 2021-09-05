60e1ef670c5e503a584b3949db67162f4776d7a0
IgorHulyaschy <gulyaschy@gmail.com>
2021-09-05 15:15:07 +0300

    added supertest jest

--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const Koa = require('koa');
+const Router = require('koa-router');
 
-const bodyParser = require("koa-bodyparser");
-const serve = require("koa-static");
-const { koaSwagger } = require("koa2-swagger-ui")
+const bodyParser = require('koa-bodyparser');
+const serve = require('koa-static');
+const { koaSwagger } = require('koa2-swagger-ui');
 const config = require('config');
+const path = require('path');
+const staticCache = require('koa-static-cache');
 
-const port = config.get('server.port')
 const app = new Koa();
 
 app.use(serve('src/docs'));
-app.use(koaSwagger({
-  routePrefix: '/docs',
-  hideTopbar: true,
-  swaggerOptions: {
-    url: `${config.get('server.baseUrl')}/docs.yml`,
-  },
-}));
+app.use(
+  koaSwagger({
+    routePrefix: '/docs',
+    hideTopbar: true,
+    swaggerOptions: {
+      url: `${config.get('server.baseUrl')}/docs.yml`,
+    },
+  }),
+);
 
 const router = new Router();
 
 app.use(bodyParser());
+app.use(
+  staticCache(path.join(__dirname, 'public'), {
+    maxAge: 365 * 24 * 60 * 60, // Add these files to caches for a year
+  }),
+);
+router.use('/', require('./url/router'));
 
-router.use("/", require("./url/router"));
 app.use(router.middleware());
-
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 
 module.exports = app;
